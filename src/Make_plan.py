03f3225ac94c552ba781a637c1529ddf25f1c6b0
wesleyjiang <jiang.yuto@husky.neu.edu>
2018-04-18 17:05:29 -0400

    plan update

--- src/Make_plan.py
+++ src/Make_plan.py
@@ -21,11 +21,15 @@ def import_guests(path):
     guest_book = Roster(guests)
     return guest_book
 
-def dominated(plan1, plan2):
+
+def compare_sol(plan1, plan2):
+    result = 0
     for key, value in plan1.items():
         if value > plan2[key]:
-            return True
-    return False
+            result += 1
+    if result == len(plan1): return "dominate"
+    if result == 0: return "dominated"
+    else: return "neither"
 
 client = MongoClient()
 db = client.plans
@@ -33,18 +37,32 @@ solutions = db.solutions
 obj1 = Objective_gender()
 obj2 = Objective_age()
 og_plan = Plan(import_guests("DS4300-Final-Project-Example-Data.csv"), 3, 5, [obj1, obj2])
-
+solutions.insert(og_plan.info())
 #print(og_plan.info())
 
 work = Working_agent(og_plan)
 
+
 #solutions.delete_many({})
 
-for i in range(5):
+for i in range(10000):
     work.random_swap()
-    solutions.insert(work.p.info())
+    sol_1 = work.p.info()['Scores']
+    to_insert = False
+    for sol in solutions.find():
+        sol_2 = sol['Scores']
+        decision = compare_sol(sol_1, sol_2)
+        if decision == "dominate":
+            solutions.delete_one(sol)
+            to_insert = True
+        if decision == "neither":
+            to_insert = True
+    if to_insert == True: solutions.insert(work.p.info())
+
+
 for document in solutions.find():
     print(document)
+    print(1)
         # if not dominated(work.p.scores):
         #     db.insert(work.p)
 
