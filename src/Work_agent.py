d87073db5b9969971e9ea485eee0e926a1f7bac9
Tim Sauchuk <sauchuk.t@husky.neu.edu>
2018-04-17 20:29:54 -0400

    lol

--- src/Work_agent.py
+++ src/Work_agent.py
@@ -22,4 +22,12 @@ class Working_agent:
         self.p.tables[rand_t_1].guests.insert(rand_s_1, g2)
         self.p.tables[rand_t_2].guests.insert(rand_s_2, g1)
 
-
+    def swap_within_table(self, tNum):
+        rand_s_1 = self.rand_seat(tNum)
+        rand_s_2 = self.rand_seat(tNum)
+        while rand_s_1 == rand_s_2:
+            rand_2 = self.rand_seat(self.rand_t())
+        g1 = self.p.tables[tNum].guests[rand_s_1]
+        g2 = self.p.tables[tNum].guests[rand_s_2]
+        self.p.tables[tNum].guests.insert(rand_s_1, g2)
+        self.p.tables[tNum].guests.insert(rand_s_2, g1)
