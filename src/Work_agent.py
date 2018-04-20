ceb57816df3c4d37452c2291d862e9d07ccabf81
Tim Sauchuk <sauchuk.t@husky.neu.edu>
2018-04-20 13:41:15 -0400

    null value handling

--- src/Work_agent.py
+++ src/Work_agent.py
@@ -16,11 +16,35 @@ class Working_agent:
         t2 = self.rand_t()
         s2 = self.rand_seat(t2)
         while s1 == s2 and t1 == t2:
-            s2 =  self.rand_seat(t2)
+            s2 = self.rand_seat(t2)
 
         t = self.p.tables
 
+        if(t[t1].guests[s1].age == 'child'):
+            if not self.good_child_seat(t1, s1, t2, s2):
+                self.random_swap()
+
         t[t1].guests[s1], t[t2].guests[s2] = t[t2].guests[s2], t[t1].guests[s1]
+
+    def good_child_seat(self, t1, s1, t2, s2):
+
+        t = self.p.tables
+
+        tableSize = t[t2].capacity
+        child = t[t1].guests[s1]
+        good_child_seat = False
+
+        if (len(t[t2].guests) > (s2 - 1) % tableSize):
+            left_of_child = t[t2].guests[(s2 - 1) % tableSize]
+            if (left_of_child.age == 'child' or left_of_child.party == child.party):
+                good_child_seat = True
+
+        if (len(t[t2].guests) > (s2 + 1) % tableSize):
+            right_of_child = t[t2].guests[(s2 + 1) % tableSize]
+            if (right_of_child.age == 'child' or right_of_child == child.party):
+                good_child_seat = True
+
+        return good_child_seat
 
     def rand_swap_within_table(self, tNum):
         rand_s_1 = self.rand_seat(tNum)
